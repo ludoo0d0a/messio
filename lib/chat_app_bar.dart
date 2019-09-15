176e2ed581815195e08aa179f8490d0399ee692d
LudoO <ludovic.valente@gmail.com>
2019-09-15 18:33:17 +0200

    add assets in pubspec create layout appbar

--- lib/chat_app_bar.dart
+++ lib/chat_app_bar.dart
@@ -9,6 +9,12 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
 
   @override
   Widget build(BuildContext context) {
+
+    var textHeading = TextStyle(color: Palette.primaryTextColor, fontSize: 20); // Text style for the name
+    var textStyle = TextStyle(color: Palette.secondaryTextColor); // Text style for everything else
+
+    double width = MediaQuery.of(context).size.width; // calculate the screen width
+
     return Material(
       child: Container(
         decoration: new BoxDecoration(boxShadow: [ //adds a shadow to the appbar
@@ -29,11 +35,65 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
                     children: <Widget>[
                       //first row containing the name and login
                       Container(
-                        child: Text('name / login'),
+                        height: 70 - (width * .06),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            // 2/8 : left icon
+                            Expanded(
+                            flex: 2,
+                            child: Center(
+                                child: Icon(
+                                  Icons.attach_file,
+                                  color: Palette.secondaryColor,
+                                ))),
+                            // 6/8 : texts on 2 rows
+                            Expanded(
+                              flex: 6,
+                              child: Container(
+                                  child: Column(
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    mainAxisSize: MainAxisSize.min,
+
+                                    children: <Widget>[
+                                      Text('Ludo Valente', style: textHeading),
+                                      Text('@ludoo0d0a', style: textStyle)
+                                    ],
+                                  )
+                              ),
+                            )
+
+                          ],
+                        )
                       ),
                       //second row containing the buttons for media (Photos / Videos / Files)
                       Container(
-                        child: Text('Photos / Videos / Files'),
+                        height: 23,
+                        padding: EdgeInsets.fromLTRB(20, 5, 5, 0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: <Widget>[
+                            Text(
+                              'Photos',
+                              style: textStyle,
+                            ),
+                            VerticalDivider(
+                              width: 30,
+                              color: Palette.primaryTextColor,
+                            ),
+                            Text(
+                              'Videos',
+                              style: textStyle,
+                            ),
+                            VerticalDivider(
+                              width: 30,
+                              color: Palette.primaryTextColor,
+                            ),
+                            Text('Files', style: textStyle)
+                          ],
+                        ),
                       )
                     ],
                   )
@@ -43,10 +103,14 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
               Expanded(
                 flex: 3,
                 child: Center(
-                  child: Text('Picture'),
+                  child: CircleAvatar(
+                    radius: (80 - (width * .06)) / 2,
+                    backgroundImage: Image.asset(
+                      Assets.user,
+                    ).image,
+                  )
                 ),
               )
-
             ],
           )
         )
