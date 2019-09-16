542d7ca37a5c2615a499581d18c0e104b8b1e4b4
LudoO <ludovic.valente@gmail.com>
2019-09-16 16:20:56 +0200

    Fix layout issue with fixed values by using flex

--- lib/widgets/ChatAppBar.dart
+++ lib/widgets/ChatAppBar.dart
@@ -5,6 +5,8 @@ import 'package:messio/config/Palette.dart';
 
 class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
 
+  const ChatAppBar();
+
   final double height = 100;
 
   @override
@@ -13,7 +15,7 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
     var textHeading = TextStyle(color: Palette.primaryTextColor, fontSize: 20); // Text style for the name
     var textStyle = TextStyle(color: Palette.secondaryTextColor); // Text style for everything else
 
-    double width = MediaQuery.of(context).size.width; // calculate the screen width
+//    double width = MediaQuery.of(context).size.width; // calculate the screen width
 
     return Material(
       child: Container(
@@ -24,9 +26,9 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
         )]),
         child: Container(
           color: Palette.primaryBackgroundColor,
+          padding: EdgeInsets.only(top: 10, bottom: 10),
           child: Row(
             children: <Widget>[
-              //These are text items
               Expanded(
                 flex: 7,
                 child: Center(
@@ -34,65 +36,71 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
                     mainAxisSize: MainAxisSize.min,
                     children: <Widget>[
                       //first row containing the name and login
-                      Container(
-                        height: 70 - (width * .06),
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: <Widget>[
-                            // 2/8 : left icon
-                            Expanded(
-                            flex: 2,
-                            child: Center(
-                                child: Icon(
-                                  Icons.attach_file,
-                                  color: Palette.secondaryColor,
-                                ))),
-                            // 6/8 : texts on 2 rows
-                            Expanded(
-                              flex: 6,
-                              child: Container(
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    mainAxisSize: MainAxisSize.min,
+                      Expanded(
+                        flex: 7,
+                        child: Container(
+//                            height: 70 - (width * .06),
+                          child: Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              // 2/8 : left icon
+                              Expanded(
+                              flex: 2,
+                              child: Center(
+                                  child: Icon(
+                                    Icons.attach_file,
+                                    color: Palette.secondaryColor,
+                                  ))),
+                              // 6/8 : texts on 2 rows
+                              Expanded(
+                                flex: 6,
+                                child: Container(
+                                    child: Column(
+                                      crossAxisAlignment: CrossAxisAlignment.start,
+                                      mainAxisAlignment: MainAxisAlignment.center,
+                                      mainAxisSize: MainAxisSize.min,
 
-                                    children: <Widget>[
-                                      Text('Ludo Valente', style: textHeading),
-                                      Text('@ludoo0d0a', style: textStyle)
-                                    ],
-                                  )
-                              ),
-                            )
+                                      children: <Widget>[
+                                        Text('Ludo Valente', style: textHeading),
+                                        Text('@ludoo0d0a', style: textStyle)
+                                      ],
+                                    )
+                                ),
+                              )
 
-                          ],
-                        )
+                            ],
+                          )
+                        ),
                       ),
                       //second row containing the buttons for media (Photos / Videos / Files)
-                      Container(
-                        height: 23,
-                        padding: EdgeInsets.fromLTRB(20, 5, 5, 0),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: <Widget>[
-                            Text(
-                              'Photos',
-                              style: textStyle,
-                            ),
-                            VerticalDivider(
-                              width: 30,
-                              color: Palette.primaryTextColor,
-                            ),
-                            Text(
-                              'Videos',
-                              style: textStyle,
-                            ),
-                            VerticalDivider(
-                              width: 30,
-                              color: Palette.primaryTextColor,
-                            ),
-                            Text('Files', style: textStyle)
-                          ],
+                      Expanded(
+                        flex: 3,
+                        child: Container(
+                          // height: 23,
+                          padding: EdgeInsets.fromLTRB(20, 5, 5, 0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: <Widget>[
+                              Text(
+                                'Photos',
+                                style: textStyle,
+                              ),
+                              VerticalDivider(
+                                width: 30,
+                                color: Palette.primaryTextColor,
+                              ),
+                              Text(
+                                'Videos',
+                                style: textStyle,
+                              ),
+                              VerticalDivider(
+                                width: 30,
+                                color: Palette.primaryTextColor,
+                              ),
+                              Text('Files', style: textStyle)
+                            ],
+                          ),
                         ),
                       )
                     ],
@@ -102,13 +110,15 @@ class ChatAppBar extends StatelessWidget implements PreferredSizeWidget {
               //This is the display picture
               Expanded(
                 flex: 3,
-                child: Center(
-                  child: CircleAvatar(
-                    radius: (80 - (width * .06)) / 2,
-                    backgroundImage: Image.asset(
-                      Assets.user,
-                    ).image,
-                  )
+                child: Container(
+                  child: Center(
+                    child: CircleAvatar(
+                      radius: 30,
+                      backgroundImage: Image.asset(
+                        Assets.user,
+                      ).image,
+                    )
+                  ),
                 ),
               )
             ],
