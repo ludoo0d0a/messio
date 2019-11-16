a90abe9ca7b5b5833f8332aeee1dc7c7b577ad2f
LudoO <ludovic.valente@gmail.com>
2019-11-16 23:39:18 +0100

    add 3 links

--- lib/widgets/ChatAppBar.dart
+++ lib/widgets/ChatAppBar.dart
@@ -10,13 +10,12 @@ import 'package:messio/config/Assets.dart';
 import 'package:messio/config/Palette.dart';
 import 'package:messio/config/Styles.dart';
 import 'package:messio/config/Transitions.dart';
+import 'package:messio/pages/AttachmentPage.dart';
 import 'package:messio/pages/ContactList.dart';
 
 import 'GradientSnackBar.dart';
 
-
 class ChatAppBar extends StatefulWidget implements PreferredSizeWidget {
-
   final Chat chat;
 
   const ChatAppBar(this.chat);
@@ -32,7 +31,6 @@ class ChatAppBar extends StatefulWidget implements PreferredSizeWidget {
 }
 
 class _ChatAppBarState extends State<ChatAppBar> {
-
   ChatBloc chatBloc;
   Chat chat;
   String _username = "";
@@ -46,6 +44,7 @@ class _ChatAppBarState extends State<ChatAppBar> {
 
   File profileImageFile;
   ImageProvider profileImage;
+
 //  AuthenticationBloc authenticationBloc;
   static const String userTitleDefault = "--userTitle";
   static const String userNicknameDefault = "--nickName";
@@ -70,117 +69,118 @@ class _ChatAppBarState extends State<ChatAppBar> {
         if (state is FetchedContactDetailsState) {
           print('Received State of Page');
           print(state.user);
-          if(state.username== chat.username){
+          if (state.username == chat.username) {
             _name = state.user.name;
             _username = state.user.username;
             _image = Image.network(state.user.photoUrl);
           }
         }
-        if(state is PageChangedState){
+        if (state is PageChangedState) {
           print(state.index);
           print('$_name, $_username');
         }
       },
-      child:  Material(
+      child: Material(
           child: Container(
-              decoration:  BoxDecoration(boxShadow: [
+              decoration: BoxDecoration(boxShadow: [
                 //adds a shadow to the appbar
-                BoxShadow(
-                    color: Colors.grey, blurRadius: 2.0, spreadRadius: 0.1)
+                BoxShadow(color: Colors.grey, blurRadius: 2.0, spreadRadius: 0.1)
               ]),
               child: Container(
                   padding: EdgeInsets.only(top: 10, bottom: 10),
                   color: Palette.primaryBackgroundColor,
                   child: Row(children: <Widget>[
                     Expanded(
-                      //we're dividing the appbar into 7 : 3 ratio. 7 is for content and 3 is for the display picture.
+                        //we're dividing the appbar into 7 : 3 ratio. 7 is for content and 3 is for the display picture.
                         flex: 7,
                         child: Center(
                             child: Column(
-                              mainAxisSize: MainAxisSize.min,
-                              children: <Widget>[
-                                Expanded(
-                                    flex: 7,
-                                    child: Container(
-                                        child: Row(
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                          children: <Widget>[
-                                            Expanded(
-                                                flex: 2,
-                                                child: Center(
-                                                    child: IconButton(
-                                                        icon: Icon(
-                                                          Icons.attach_file,
-                                                          color:
-                                                          Palette.secondaryColor,
-                                                        ),
-                                                        onPressed: () => showAttachmentBottomSheet(context),
-                                                        ))),
-                                            Expanded(
-                                                flex: 6,
-                                                child: Container(child:
-                                                BlocBuilder<ChatBloc, ChatState>(
-                                                    builder: (context, state) {
-                                                      return Column(
-                                                        crossAxisAlignment:
-                                                        CrossAxisAlignment.start,
-                                                        mainAxisAlignment:
-                                                        MainAxisAlignment.center,
-                                                        mainAxisSize: MainAxisSize.min,
-                                                        children: <Widget>[
-                                                          Text(_name,
-                                                              style: Styles.textHeading),
-                                                          Text('@$_username',
-                                                              style: Styles.text)
-                                                        ],
-                                                      );
-                                                    }))),
-                                          ],
+                          mainAxisSize: MainAxisSize.min,
+                          children: <Widget>[
+                            Expanded(
+                                flex: 7,
+                                child: Container(
+                                    child: Row(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: <Widget>[
+                                    Expanded(
+                                        flex: 2,
+                                        child: Center(
+                                            child: IconButton(
+                                          icon: Icon(
+                                            Icons.attach_file,
+                                            color: Palette.secondaryColor,
+                                          ),
+                                          onPressed: () => showAttachmentBottomSheet(context),
                                         ))),
-                                //second row containing the buttons for media
-                                Expanded(
-                                    flex: 3,
-                                    child: Container(
-                                        padding: EdgeInsets.fromLTRB(20, 5, 5, 0),
-                                        child: Row(
-                                          mainAxisAlignment:
-                                          MainAxisAlignment.start,
-                                          crossAxisAlignment:
-                                          CrossAxisAlignment.center,
-                                          children: <Widget>[
-                                            Text(
-                                              'Photos',
-                                              style: Styles.text,
-                                            ),
-                                            VerticalDivider(
-                                              width: 30,
-                                              color: Palette.primaryTextColor,
-                                            ),
-                                            Text(
-                                              'Videos',
-                                              style: Styles.text,
-                                            ),
-                                            VerticalDivider(
-                                              width: 30,
-                                              color: Palette.primaryTextColor,
-                                            ),
-                                            Text('Files', style: Styles.text)
-                                          ],
-                                        ))),
-                              ],
-                            ))),
+                                    Expanded(
+                                        flex: 6,
+                                        child: Container(
+                                            child: BlocBuilder<ChatBloc, ChatState>(builder: (context, state) {
+                                          return Column(
+                                            crossAxisAlignment: CrossAxisAlignment.start,
+                                            mainAxisAlignment: MainAxisAlignment.center,
+                                            mainAxisSize: MainAxisSize.min,
+                                            children: <Widget>[
+                                              Text(_name, style: Styles.textHeading),
+                                              Text('@$_username', style: Styles.text)
+                                            ],
+                                          );
+                                        }))),
+                                  ],
+                                ))),
+                            //second row containing the buttons for media
+                            Expanded(
+                                flex: 3,
+                                child: Container(
+                                    padding: EdgeInsets.fromLTRB(20, 5, 5, 0),
+                                    child: Row(
+                                      mainAxisAlignment: MainAxisAlignment.start,
+                                      crossAxisAlignment: CrossAxisAlignment.center,
+                                      children: <Widget>[
+                                        GestureDetector(
+                                          child: Text(
+                                            'Photos',
+                                            style: Styles.text,
+                                          ),
+                                          onTap: () => Navigator.push(context,
+                                              SlideLeftRoute(page: AttachmentPage(this.chat.chatId, FileType.IMAGE))),
+                                        ),
+                                        VerticalDivider(
+                                          width: 30,
+                                          color: Palette.primaryTextColor,
+                                        ),
+                                        GestureDetector(
+                                          child: Text(
+                                            'Videos',
+                                            style: Styles.text,
+                                          ),
+                                          onTap: () => Navigator.push(context,
+                                              SlideLeftRoute(page: AttachmentPage(this.chat.chatId, FileType.VIDEO))),
+                                        ),
+                                        VerticalDivider(
+                                          width: 30,
+                                          color: Palette.primaryTextColor,
+                                        ),
+                                        GestureDetector(
+                                          child: Text('Files', style: Styles.text),
+                                          onTap: () => Navigator.push(context,
+                                              SlideLeftRoute(page: AttachmentPage(this.chat.chatId, FileType.ANY))),
+                                        )
+                                      ],
+                                    ))),
+                          ],
+                        ))),
                     //This is the display picture
                     Expanded(
                         flex: 3,
-                        child: Container(child: Center(child:
-                        BlocBuilder<ChatBloc, ChatState>(
-                            builder: (context, state) {
-                              return CircleAvatar(
-                                radius: 30,
-                                backgroundImage: _image.image,
-                              );
-                            })))),
+                        child:
+                            Container(child: Center(child: BlocBuilder<ChatBloc, ChatState>(builder: (context, state) {
+                          return CircleAvatar(
+                            radius: 30,
+                            backgroundImage: _image.image,
+                          );
+                        })))),
                   ])))),
     );
   }
@@ -198,43 +198,31 @@ class _ChatAppBarState extends State<ChatAppBar> {
     authenticationBloc.dispatch(ClickedLogout());
   }
 
-  showAttachmentBottomSheet(context){
+  showAttachmentBottomSheet(context) {
     showModalBottomSheet(
         context: context,
-        builder: (BuildContext bc){
+        builder: (BuildContext bc) {
           return Container(
-            child:  Wrap(
+            child: Wrap(
               children: <Widget>[
+                ListTile(leading: Icon(Icons.image), title: Text('Image'), onTap: () => showFilePicker(FileType.IMAGE)),
                 ListTile(
-                    leading:  Icon(Icons.image),
-                    title:  Text('Image'),
-                    onTap: () => showFilePicker(FileType.IMAGE)
-                ),
+                    leading: Icon(Icons.videocam), title: Text('Video'), onTap: () => showFilePicker(FileType.VIDEO)),
                 ListTile(
-                    leading:  Icon(Icons.videocam),
-                    title:  Text('Video'),
-                    onTap: () => showFilePicker(FileType.VIDEO)
-                ),
-                ListTile(
-                  leading:  Icon(Icons.insert_drive_file),
-                  title:  Text('File'),
+                  leading: Icon(Icons.insert_drive_file),
+                  title: Text('File'),
                   onTap: () => showFilePicker(FileType.ANY),
                 ),
               ],
             ),
           );
-        }
-    );
+        });
   }
 
   showFilePicker(FileType fileType) async {
     File file = await FilePicker.getFile(type: fileType);
-    chatBloc.dispatch(SendAttachmentEvent(chat.chatId,file,fileType));
+    chatBloc.dispatch(SendAttachmentEvent(chat.chatId, file, fileType));
     Navigator.pop(context);
     GradientSnackBar.showMessage(context, 'Sending attachment..');
   }
-
-
 }
-
-
